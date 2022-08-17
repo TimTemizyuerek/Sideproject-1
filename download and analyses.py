8b0896632a35d4301438384b1d2d2b243ebec2f5
Tim Temizyürek <57264657+TimTemizyuerek@users.noreply.github.com>
2022-08-17 13:13:37 +0200

    Python changes?
    I just opened the whole thing in python, hopefully the damage isn't fatal :)

--- download and analyses.py
+++ download and analyses.py
@@ -1,8 +1,13 @@
 ## download
-download archive files
+## download archive files
+
+
+## load data into python
+import textract
+text = textract.process("path/to/file.extension")
 
 ## extract PhD positions
-whatever
+
 
 ## look for words (gender)
 
