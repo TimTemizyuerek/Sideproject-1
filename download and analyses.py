83a45f0d77fcb950a8e9cf652caf9fdbf199f438
Tim Temizyürek <57264657+TimTemizyuerek@users.noreply.github.com>
2022-08-17 15:02:52 +0200

    Extracts PhD section
    Loads a pdf, finds pages with PhD section, creates a new PDF containing only the PhD positions
    STILL TO FIX: the loop does not end for some reason.

--- download and analyses.py
+++ download and analyses.py
@@ -1,10 +1,44 @@
-## download
-## download archive files
+# download
+
+## import pdf files
+import PyPDF2
+
+# load the pdf, make it a pdf object (don't really know what that is)
+runner_pdf = open('C:/Users/timte/Downloads/Mnth_Review_Jul_22.pdf', 'rb')
+runner_pdf = PyPDF2.PdfFileReader(runner_pdf)
+
+# get first page (to extract the PhD position pages from)
+first_page = runner_pdf.getPage(0)
+first_page_raw = first_page.extractText()
+
+## identify start page of PhD positions from first page
+starter_PhDpage_str = first_page_raw.find("\nJobs")
+starter_PhDpage = int(first_page_raw[(starter_PhDpage_str-2):(starter_PhDpage_str)])
+
+## identify end page of PhD positions from first page
+ender_PhDpage_str = first_page_raw.find("\nOther")
+ender_PhDpage = int(first_page_raw[(ender_PhDpage_str-2):(ender_PhDpage_str)])
+
+## extract pages with PhD positions
+from PyPDF2 import PdfFileWriter
+
+## use shorter variables for the loop
+writer = PdfFileWriter()
+start = starter_PhDpage-1
+end = ender_PhDpage-1
+
+## loop over all pages to extract
+while start <= end:
+    ## add pages one by one
+    writer.addPage(runner_pdf.getPage(start))
+    ## increase counter
+    start = start + 1
+
+## print the resulting PDF file
+with open("PhD_positions.pdf", "wb") as out:
+    writer.write(out)
 
 
-## load data into python
-import textract
-text = textract.process("path/to/file.extension")
 
 ## extract PhD positions
 
