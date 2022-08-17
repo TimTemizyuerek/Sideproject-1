3211e0e94416c34116115b0f35a1e1af2a2c6f5f
Akhila1498 <111276835+Akhila1498@users.noreply.github.com>
2022-08-17 15:28:04 +0200

    Update download and analyses.py

--- download and analyses.py
+++ download and analyses.py
@@ -1,4 +1,22 @@
 # download
+import os
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+url = "https://evol.mcmaster.ca/cgi-bin/my_wrap/brian/evoldir/Archive/"
+
+#If there is no such folder, the script will create one automatically
+folder_location = r'C:/Users/akhil/Desktop'
+if not os.path.exists(folder_location):os.mkdir(folder_location)
+
+response = requests.get(url)
+soup= BeautifulSoup(response.text, "html.parser")     
+for link in soup.select("a[href$='.pdf']"):
+    #Name the pdf files using the last portion of each link which are unique in this case
+    filename = os.path.join(folder_location,link['href'].split('/')[-1])
+    with open(filename, 'wb') as f:
+        f.write(requests.get(urljoin(url,link['href'])).content)
 
 ## import pdf files
 import PyPDF2
