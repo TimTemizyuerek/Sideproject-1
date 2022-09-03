c17ee872222ae9efa8f5150c0b07f95859b6e145
Tim Temizyürek <57264657+TimTemizyuerek@users.noreply.github.com>
2022-09-03 15:14:23 +0200

    updated the first part of the loop

--- download and analyses - tim.py
+++ download and analyses - tim.py
@@ -27,7 +27,7 @@ for link in soup.select("a[href$='.pdf']"):
 for n in fnmatch.filter(os.listdir(folder_location), '*.pdf*'):
 
     ## load individual file and make it a pdf object
-    runner_file_location = (folder_location + "/" + fnmatch.filter(os.listdir(folder_location), '*.pdf*')[n])
+    runner_file_location = (folder_location + "/" + fnmatch.filter(os.listdir(folder_location), '*.pdf*')[1])
     runner_file = open(runner_file_location, 'rb')
     runner_pdf = PyPDF2.PdfFileReader(runner_file)
 
@@ -56,16 +56,16 @@ for n in fnmatch.filter(os.listdir(folder_location), '*.pdf*'):
     extract_first_page_raw = writer.getPage(0).extractText()
 
     ## start of PhD position overview
-    starter_overview_str = extract_first_page_raw.find("GradStudentPositions") + 21
+    starter_overview_int = int(extract_first_page_raw.find("GradStudentPositions") + 21)
 
     ## end of PhD positions overview
-    ## take the first 14 char of the first position name
-    first_PhD_position = extract_first_page_raw[starter_overview_str:starter_overview_str + 35]
-    all_occurances = [i for i in range(len(extract_first_page_raw)) if extract_first_page_raw.startswith(first_PhD_position, i)]
-    ender_overview_str = all_occurances[1]
+    ## take the first 15 char of the first position name
+    first_PhD_position = extract_first_page_raw[starter_overview_int:starter_overview_int + 15]
+    ## find second instance (to identify end of the PhD overview on page 1
+    ender_overview_int = int(extract_first_page_raw.find(first_PhD_position,starter_overview_int+len(first_PhD_position),))
 
     ## extract overview and count the numbers, to get the numbers of positions
-    overview_str = extract_first_page_raw[starter_overview_str:ender_overview_str]
+    overview_str = extract_first_page_raw[starter_overview_int:ender_overview_int]
 
     ## counts all instances where a number is preceded by ". 1", ". 2", and ". 3"
     ## thus counting all positions starting on pages 10-39 (that might need adjustment?)
@@ -73,15 +73,30 @@ for n in fnmatch.filter(os.listdir(folder_location), '*.pdf*'):
                              len([i for i in range(len(overview_str)) if overview_str.startswith(". 2", i)]) + \
                              len([i for i in range(len(overview_str)) if overview_str.startswith(". 3", i)])
 
+    ## create a list with the names of the PhD positions
+    ## identify start of positions
+    helper_indices = [i for i in range(len(overview_str)) if overview_str.startswith("\n", i)]
+
+    ## create list to store names in
+    PhD_names = [None] * (total_number_positions - 1)
+
+    ## extract indices
+    counter = 0
+    for m in helper_indices:
+        PhD_names[counter] = overview_str[m + 1: m + 15]
+        counter = counter + 1
+
+    ## remove last entry (which is empty)
+    PhD_names.pop()
+
+    ## add first PhD_position
+    PhD_names.insert(0, first_PhD_position)
+
+    for m in PhD_names:
 
 
-    ## your part starts here:
-    ## you have the PhD positions en block in "writer"
-    ## you have the number of PhD positions in "total_number_positions"
 
-    for variable in total_number_positions:
 
-        ## have fun :)
 
 
 ## plot
