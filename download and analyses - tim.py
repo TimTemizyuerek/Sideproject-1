b4a5219078d7a4bcc9d8393e3727cac05e7b6522
Tim Temizyürek <57264657+TimTemizyuerek@users.noreply.github.com>
2022-12-07 15:39:52 +0100

    Search within PhD position possible
    PhD positions are separated (untested) and words can be searched for.

--- download and analyses - tim.py
+++ download and analyses - tim.py
@@ -10,7 +10,7 @@ import PyPDF2  ## handles pdf files
 from PyPDF2 import PdfFileWriter ## handles pdf file
 import fnmatch ## count files in directory
 import re
-import pandas
+import pandas as pd
 
 ## custom functions
 
@@ -58,31 +58,39 @@ for link in soup.select("a[href$='.pdf']"):
 ##     ## do the transformation
 ##     pdf_to_txt(runner_file_location)
 
+
+## create output dataframe
+
+file_ID, PhD_ID, totalwordcount, word1, word2, ... wordn
+
+output_df = pd.DataFrame(columns = ['fileID', 'PhDID', 'word1', 'word2', 'word3'],index = ['R1', 'R2', 'R3', 'R4', 'R5'])
+
 ## extract PhD position section from all files
 for n in fnmatch.filter(os.listdir(folder_location), '*.txt*'):
 
     ## load individual file and make into a txt object
     runner_file_location = (folder_location + "/" + fnmatch.filter(os.listdir(folder_location), '*.txt*')[n])
-    runner_file = open(runner_file_location, 'r')
+    runner_file = open(runner_file_location, 'r', encoding="utf8")
     all_in_one_string = runner_file.read()
+    all_in_one_string = all_in_one_string.replace(" ","")
     
     ## find second "GradStudentPositions"
     second_GradStudentPositions_index = [i for i in range(len(all_in_one_string)) if all_in_one_string.startswith("\nGradStudentPositions", i)][1]
     ## find second "Jobs"
     second_Jobs_index = [i for i in range(len(all_in_one_string)) if all_in_one_string.startswith("\nJobs", i)][1]
     
-    ## extract relevant text
+    ## extract full PhD text
     all_GradStudentPositions_string = all_in_one_string[second_GradStudentPositions_index:second_Jobs_index]
-
+    
     ## no name can be after this index (problem: miss lasts position)
-    end_of_PhD_names = [i for i in range(len(all_GradStudentPositions_string)) if all_GradStudentPositions_string.startswith(" . . . ", i)][-1]
+    end_of_PhD_names = [i for i in range(len(all_GradStudentPositions_string)) if all_GradStudentPositions_string.startswith(".....", i)][-1]
 
     ## extract table of content
     PhD_table_of_content = all_GradStudentPositions_string[:end_of_PhD_names]
     
     ## find instances of \n to cut up PhD positions (problem: what if the position does not start with a \n)
     indices_of_PhD_start_and_end_points = [i for i in range(len(PhD_table_of_content)) if PhD_table_of_content.startswith("\n", i)][1:]
-        
+    
     ## extract PhD position names
     PhD_names = [None]*(len(indices_of_PhD_start_and_end_points)-1)
     for n in range(len(indices_of_PhD_start_and_end_points)-1):
@@ -90,108 +98,34 @@ for n in fnmatch.filter(os.listdir(folder_location), '*.txt*'):
         ## nth position
         runner = str(PhD_table_of_content[indices_of_PhD_start_and_end_points[n]:indices_of_PhD_start_and_end_points[n+1]])
         
-        ## find first and last index (problem: if the PhD name is longer than the page, there are additional \ns)
+        ## find first instance of "\n"
         runner_first = runner.find("\\n")
-        runner_last = runner.find(" .")
+        ## find first instance of ".."
+        runner_last = runner.find("..")
         
-        ## extract clean name
+        ## extract clean names
         PhD_names[n] = runner[runner_first+2:runner_last]
     
-    ## extract full PhD texts
+    ## create a version of the text without \n, as it makes searching for PhD positions impossible
+    clean_all_GradStudentPositions_string = all_GradStudentPositions_string.replace("\n","")
+    ## get rid of spaces as well
+    clean_all_GradStudentPositions_string = clean_all_GradStudentPositions_string.replace(" ","")
 
-    ## WIP
-    for n in range(len(PhD_names)):
+    ## extract full PhD texts and look for words
+    for n in range(len(PhD_names)-1):
 
-        [i for i in range(len(all_GradStudentPositions_string)) if all_GradStudentPositions_string.startswith(PhD_names[n], i)][-1]    
-
-        runner_start = all_GradStudentPositions_string.find(PhD_names[n])
-        runner_last = all_GradStudentPositions_string.find(PhD_names[n+1])
-
-    ## bookmark
-
-
-
-
-
-
-
-
-
-    all_GradStudentPositions_string[1:800]
-    var = [i for i in range(len(all_in_one_string)) if all_in_one_string.startswith(b" .", i)]
-
-
-    ## start of PhD position overview
-    starter_overview_int = int(extract_first_page_raw.find("GradStudentPositions") + 21)
-
-    ## end of PhD positions overview
-
-    ## stop at the dots instead
-
-    ## take the first 15 char of the first position name
-    first_PhD_position = extract_first_page_raw[starter_overview_int:starter_overview_int + 15]
-    ## find second instance (to identify end of the PhD overview on page 1
-    ender_overview_int = int(extract_first_page_raw.find(first_PhD_position,starter_overview_int+len(first_PhD_position),))
-
-    ## extract overview and count the numbers, to get the numbers of positions
-    overview_str = extract_first_page_raw[starter_overview_int:ender_overview_int]
-
-    ## counts all instances where a number is preceded by ". 1", ". 2", and ". 3"
-    ## thus counting all positions starting on pages 10-39 (that might need adjustment?)
-    total_number_positions = len([i for i in range(len(overview_str)) if overview_str.startswith(". 1", i)]) + \
-                             len([i for i in range(len(overview_str)) if overview_str.startswith(". 2", i)]) + \
-                             len([i for i in range(len(overview_str)) if overview_str.startswith(". 3", i)])
-
-    ## create a list with the names of the PhD positions
-    ## identify start of positions
-    helper_indices = [i for i in range(len(overview_str)) if overview_str.startswith("\n", i)]
-
-    ## create list to store names in
-    PhD_names = [None] * (total_number_positions - 1)
-
-    ## extract indices
-    counter = 0
-    for m in helper_indices:
-        PhD_names[counter] = overview_str[m + 1: m + 15]
-        counter = counter + 1
-
-    ## remove last entry (which is empty)
-    PhD_names.pop()
-
-    ## add first PhD_position
-    PhD_names.insert(0, first_PhD_position)
-
-    ## extract PhD adverts m = PhD_names[0]
-    
-    PhD_start_page = [None] * (total_number_positions - 1)
-    PhD_end_page = [None] * (total_number_positions - 1)
-    PhD_start_position = [None] * (total_number_positions - 1)
-    PhD_end_position = [None] * (total_number_positions - 1)
-    
-    for m in list(range(0,len(PhD_names))):
-
-        runner_PhD_names = PhD_names[m]
-
-        # extract text and do the search
-        for i in list(range(0, writer.getNumPages())):
-            pageObj = writer.getPage(i)
-            text = pageObj.extractText() 
-            if re.search(runner_PhD_names, text):
-                PhD_start_page[m] = i
-                PhD_start_position[m] = re.search(runner_PhD_names, text)
-                
+        ## start point
+        runner_first = [i for i in range(len(clean_all_GradStudentPositions_string)) if clean_all_GradStudentPositions_string.startswith(PhD_names[n], i)][1]
+        ## end point
+        runner_last = [i for i in range(len(clean_all_GradStudentPositions_string)) if clean_all_GradStudentPositions_string.startswith(PhD_names[n+1], i)][1]
         
-        re.search(runner_PhD_names, text)
+        ## extract full PhD text
+        runner_PhD = clean_all_GradStudentPositions_string[runner_first:runner_last]
 
-        
-        
-        
-
-    
-
-    ## pdf_file_location = runner_file_location
+        ## search within PhD text
+        words_to_look_for = "research"
+        print(len([i for i in range(len(runner_PhD)) if runner_PhD.startswith(words_to_look_for, i)]))
 
 
+    ## build output row
 
-
-## plot
